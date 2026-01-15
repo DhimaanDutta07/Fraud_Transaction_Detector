485e70ea9ee16b09b3f304b89e2fa50d26ee9c84
Dhimaan Dutta <dhimaanmams.07@gmail.com>
2026-01-15 17:32:25 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv('MONGO_URI','mongodb://localhost:27017/')
+    MONGO_URI = os.getenv('MONGO_URI')
     DATABASE_NAME = 'Regal'
     SECRET_KEY = os.getenv('SECRET_KEY','your-secret-key-here')
     FRAUD_THRESHOLD = 0.5
